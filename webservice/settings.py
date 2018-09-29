4dd89a67b8c7f5181edc8337f7233fe6351b6877
Jorge <jorgedguezm@gmail.com>
2018-09-29 21:58:35 +0200

    Delete temporary files

--- webservice/settings.py
+++ webservice/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'general_information.apps.GeneralInformationConfig'
+    'general_information.apps.ElectionsConfig'
 ]
 
 MIDDLEWARE = [
